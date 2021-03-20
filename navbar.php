646ef846c2e8a85faac3953325a1736304c4bb32
guddus326 <guddus326@naver.com>
2021-03-21 01:30:09 +0900

    qna 기능 완성

---
+++ navbar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>App&Me</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500;1,800&display=swap" rel="stylesheet">
+    <style>
+        .navbar-fixed-top { background-color: #ffffff; }
+    </style>
+  </head>
+  <body>
+
+    <nav class="navbar-fixed-top navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">
+                    <img alt="Brand" style="margin-top:-10px" src="./img/appandme_web_logo.png">
+                  </a>
+              </div>
+             
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"> <span class="sr-only">(current)</span></li>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="top.php">부원소개</a></li>
+              <li><a href="#">지원하기</a></li>
+              <li><a href="#">Q&A</a></li>
+            </ul>
+        
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+  </body>
+</html>
--- qnaaction.php
+++ qnaaction.php
@@ -2,14 +2,14 @@
 
 include './db_conn.php';
 
-$username = $_POST['name '];
+$name = $_POST['name'];
 $title = $_POST['title'];
 $content = $_POST['content'];
 
-$sql="insert into qna(username,title,content)values('$username','$title','$content')";
+$sql="insert into qna(title,name,content)values('$title','$name','$content')";
 mysqli_query($conn,$sql);
 mysqli_close($conn);
 
-echo("<script>location.href='size.php';</script>"); 
+echo("<script>location.href='index.php';</script>"); 
 ?>
 
