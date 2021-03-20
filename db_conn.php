dcd6396dabfce4af2c66c18c80ebf90a5aa71db1
guddus326 <guddus326@naver.com>
2021-03-21 00:06:38 +0900

    디비 연결

---
+++ db_conn.php
@@ -0,0 +1,8 @@
+<?php
+$conn=mysqli_connect('localhost','test','1111','App_and_Me');
+if($conn){
+   // echo "mysql database에 연결되었습니다";
+}else{
+   // echo "mysql database에 연결 실패";
+}
+?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include './db_conn.php';
 ?>
 <!DOCTYPE html>
 <html>
