ec79ecb6e5e0ead197b908b496c0c4d4c31e8477
KimAra <s2019w06@e-mirim.hs.kr>
2021-03-21 22:21:44 +0900

    dothome connect

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','test','1111','App_and_Me');
+$conn=mysqli_connect('112.175.184.73','appandme','prettygirls18!','appandme');
 if($conn){
    // echo "mysql database에 연결되었습니다";
 }else{
