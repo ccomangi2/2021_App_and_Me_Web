f65bf027178ae9543bf073a43d1160cf4fbca227
맹수연 <sooyeon030908@gmail.com>
2021-03-21 14:43:28 +0900

    전체적인 화면 구현

--- qna_write.php
+++ qna_write.php
@@ -23,39 +23,56 @@
     <hr width="60%" color="#FF6384">
   </center>
   <div class="margin">
-    <div class="box">
-      <form class="login-form" action="qnaaction.php" method="post">
-        <table class="table table-boardered">
-          <tr class="content">
-            <td>학번 이름</td>
-            <td><input type="text" class="form-control" name="name" style="width:200px;display:inline;">
-            <td>
-          </tr>
-
-          <tr class="content">
-            <td>제목</td>
-            <td><input type="text" class="form-control" name="title" style="width:200px;display:inline;"></td>
-          </tr>
-          <tr class="content">
-            <td>내용</td>
-            <td><input type="text" class="form-control" name="content" style="width:200px;display:inline;"></td>
-          </tr>
-
-          <tr>
-            <td>
-              <!-- 수정필요 -->
-              <div class="apply">
-                <input type="submit">
-                <!-- <input type="submit" class="btn btn-primary btn-lg"> -->
-                <span onclick="location.href='qnaaction.php'">등록하기</span>
-              </div>
-            </td>
-          </tr>
-
-        </table>
-      </form>
-    </div>
+    <center>
+      <div class="box">
+        <form class="login-form" action="qnaaction.php" method="post">
+          <table width="95%" class="list-table">
+            <tr class="content">
+              <td>제목</td>
+              <td><input type="text" name="title" size="50"
+                  style="float:left; margin-left: 5%; width: 69.5%; height:50px">
+              </td>
+            </tr>
+            <tr class="content">
+              <td>학번 이름</td>
+              <td><input type="text" name="name" size="50"
+                  style="float:left; margin-left: 5%; width: 33%; height:50px" placeholder="3100 김미림" required>
+              </td>
+            </tr>
+            <tr> 
+              <td colspan="10">
+                <hr>
+              </td>
+            </tr>
+            <tr class="content">
+              <td>내용</td>
+              <td colspan="2">
+                <textarea name="content" onkeyup="len_chk()" style="width:92%; float:left; margin-left: 5%;"
+                  rows="6" placeholder="내용을 입력해주세요. (200자 이내)" required></textarea>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
+      <input type="submit" class="apply" value="등록하기" onclick="location.href='qnaaction.php'">
+    </center>
   </div>
+  <script language="javascript">
+    //글자수 제한 체크 
+    function len_chk() {
+      var frm = document.apply.reason;
+      var frm2 = document.apply.todo;
+      if (frm.value.length > 400) {
+          alert("글자수는 200자로 제한됩니다.");
+          frm.value = frm.value.substring(0, 400);
+          frm.focus();
+      } else if (frm2.value.length > 400) {
+          alert("글자수는 200자로 제한됩니다.");
+          frm2.value = frm2.value.substring(0, 400);
+          frm2.focus();
+      }
+    }
+  </script>
 </body>
 
 </html>
