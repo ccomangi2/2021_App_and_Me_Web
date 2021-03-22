18d7ca68d163c5f614de171581860ff090ebf981
맹수연 <sooyeon030908@gmail.com>
2021-03-22 22:11:17 +0900

    qna 화면 완료

--- qna.php
+++ qna.php
@@ -36,37 +36,41 @@
           <th id="head">제목</th>
         </tr>
       </thread>
-      <tbody align="center" bgcolor="#ffffff">
-        <tr id="line">
+      <tbody bgcolor="#ffffff">
+        <tr id="line" align="center">
           <td id="no" class="no1_Q">01</td>
-          <td class="no1_Q">어떤 활동을 하나요?</td>
+          <td class="no1_Q">앱앤미는 어떤 활동을 하나요?</td>
         </tr>
-        <tr id="line">
-          <td class="no1_A" colspan="2" style="display:none;">답</td>
+        <tr id="line" align="left">
+          <td class="no1_A" colspan="2" style="display:none;">
+          <span>&nbsp;A.&nbsp;&nbsp;</span>다양한 외부 공모전과 앱/아이디어 경진 대회에 참가할 수 있고, 특강/세미나 등 여러 가지 행사에도 참여할 수 있습니다. 그 외에도 동아리 내에서 전공 관련 스터디를 진행합니다.</td>
         </tr>
 
-        <tr id="line">
+        <tr id="line" align="center">
           <td id="no" class="no2_Q">02</td>
           <td class="no2_Q">모집 기간은 언제 인가요?</td>
         </tr>
-        <tr id="line">
-          <td class="no2_A" colspan="2" style="display:none;">답</td>
+        <tr id="line" align="left">
+          <td class="no2_A" colspan="2" style="display:none;">
+          <span>&nbsp;A.&nbsp;&nbsp;</span>2021년 03월 23일 부터 2021년 04월 02일까지 입니다.</td>
         </tr>
 
-        <tr id="line">
+        <tr id="line" align="center">
           <td id="no" class="no3_Q">03</td>
           <td class="no3_Q">면접은 언제 보나요?</td>
         </tr>
-        <tr id="line">
-          <td class="no3_A" colspan="2" style="display:none;">답</td>
+        <tr id="line" align="left">
+          <td class="no3_A" colspan="2" style="display:none;">
+          <span>&nbsp;A.&nbsp;&nbsp;</span>2021년 03월 27일 토요일에 zoom으로 면접 진행합니다.</td>
         </tr>
 
-        <tr id="line">
+        <tr id="line" align="center">
           <td id="no" class="no4_Q">04</td>
           <td class="no4_Q">몇 명 뽑나요?</td>
         </tr>
-        <tr id="line">
-          <td class="no4_A" colspan="2" style="display:none;">답</td>
+        <tr id="line" align="left">
+          <td class="no4_A" colspan="2" style="display:none;">
+          <span>&nbsp;A.&nbsp;&nbsp;</span>디자인과는 2명, 개발과는 과 상관없이 4명으로 총 6명 뽑습니다.</td>
         </tr>
       </tbody>
     </table>
